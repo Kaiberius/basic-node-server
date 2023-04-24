20914d5a136846fa7ff4328df798b38a79713f47
Kartier <115204550+Kaiberius@users.noreply.github.com>
2023-04-24 06:59:57 +0000

    supreme

--- node.js
+++ node.js
@@ -7,6 +7,11 @@ const port = 1616;
 
 const server = http.createServer((req, res => {
     if (req.url === "/") {
-        const html = fs
+        const html = fs.readFileSync(path.join(__dirname, "index.html"));
+        res.statusCode = 200;
+        res.setHeader("content-type", "text/html");
+        res.end(html)
+    } else {
+        res.statusCode
     }
 }))
